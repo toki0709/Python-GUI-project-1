7ea0b7adbea581e057ee57c65b45256b6338f3fe
Toki Tazwar <tokitazwar71@gmail.com>
2020-07-22 16:02:07 +0200

    pq diagram app

--- pq_frontend.py
+++ pq_frontend.py
@@ -4,32 +4,59 @@ from tkinter import *
 from tkinter.ttk import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
-# importing askopenfile function
-# from class filedialog
 from tkinter.filedialog import askopenfile
+import pandas as pd
+import matplotlib.pyplot as plt
 
 root = Tk()
 root.geometry('400x250')
 root.title("8p2-certification GmbH")
 root.iconbitmap("favicon.ico")
-# This function will be used to open
-# file in read mode and only Python files
-# will be opened
+
 """file = askopenfile(mode='r', filetypes=[  # initialdir="C:/Users/nn/Downloads",
         ('CSV Files', '*.csv'), ('All Files', '*.*')])"""
 
 
 def open_file():
-    file = askopenfile(mode='r', initialdir="C:/Users/nn/Downloads", filetypes=[
+    global file
+    file = askopenfile(initialdir="C:/Users/nn/Downloads", filetypes=[
         ('CSV Files', '*.csv'), ('All Files', '*.*')])
     """root.filename = filedialog.askopenfilename( mode='r',
     initialdir='C:/Users/nn/Downloads', title='Open File', filetypes=(("CSV File", "*.csv"), ("All Files", "*.*")))"""
 
 
+def see_graph():
+
+    df = pd.read_csv(file)
+    df.reset_index(drop=True)
+
+    df.columns = ['Real Power', 'Reactive Power Plant', 'Reactive Power Grid']
+
+    #columns = df.columns
+    #index = df.index
+    #values = df.values
+
+    y_axis = df['Real Power']
+    rpg = df['Reactive Power Plant']
+    rpp = df['Reactive Power Grid']
+
+    plt.plot(rpp, y_axis, '-b', label='Real Power(P)')
+    plt.plot(rpg, y_axis, '-r',  label='Reactive Power(Q)')
+    plt.ylabel('P in p.u.', fontsize=12, fontfamily='Arial')
+    plt.xlabel('Q in MVar', fontsize=12, fontfamily='Arial')
+    plt.legend(loc='best')
+    plt.title('PQ Diagram')
+    plt.grid()
+    plt.show()
+    print(see_graph())
+
+
 button_open = Button(root, text='Open', command=lambda: open_file())
 button_open.grid(row=2, column=1)
+button_open = Button(root, text='Plot Graph', command=lambda: see_graph())
+button_open.grid(row=3, column=1)
 button_exit = Button(root, text='Exit', command=root.quit)
-button_exit.grid(row=3, column=1)
+button_exit.grid(row=4, column=1)
 
 
 mainloop()
